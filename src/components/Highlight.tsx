75182e3f73102029865bdb70c054f0f4fe6dd0de
Ronald Gustavo <Ronaldgustavo.rg@Gmail.com>
2025-08-12 20:36:30 +0700

    feat: update code

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,47 +1,57 @@
+import { FaSlack } from "react-icons/fa";
+import { userImg } from "../assets/images";
+import { MdDelete } from "react-icons/md";
+
 const Highlight = () => {
   const dataLeft = [
     {
-      title: 'Edit your Taskboard',
+      title: "Edit your Taskboard",
       description:
-        'Transform your environment to enhance productivity and creativity customize layout.',
+        "Transform your environment to enhance productivity and creativity customize layout.",
     },
 
     {
-      title: 'Edit your Workspace',
+      title: "Edit your Workspace",
       description:
-        'Transform your environment to enhance productivity and creativity customize layout.',
+        "Transform your environment to enhance productivity and creativity customize layout.",
     },
   ];
 
   const dataMid = [
     {
-      iconLogo: 'icon',
-      title: 'Development Application Android',
-      subTitle: 'Slack - Engineer',
-      percentage: '80',
-      dayLeft: '5',
-      iconProfile: 'iconp',
-      startDay: '14',
+      title: "Development Application Android",
+      subTitle: "Slack - Engineer",
+      percentage: "80",
+      dayLeft: "5",
+      iconProfile: "iconp",
+      startDay: "14",
     },
     {
-      iconLogo: 'icon',
-      title: 'Development Application Ios',
-      subTitle: 'Slack - Product',
-      percentage: '50',
-      dayLeft: '2',
-      iconProfile: 'iconp',
-      startDay: '20',
+      title: "Development Application Ios",
+      subTitle: "Slack - Product",
+      percentage: "50",
+      dayLeft: "2",
+      iconProfile: "iconp",
+      startDay: "20",
     },
   ];
 
   const dataRight = [
     {
-      title: 'Grooming Meeting',
-      date: '01.00 - 02.00 pm',
+      title: "Grooming Meeting",
+      date: "01.00 - 02.00 pm",
     },
     {
-      title: 'Technical Meeting',
-      date: '09.00 - 10.00 pm',
+      title: "Technical Meeting",
+      date: "09.00 - 10.00 pm",
+    },
+    {
+      title: "Scoping Meeting",
+      date: "03.00 - 04.00 pm",
+    },
+    {
+      title: "Handover Meeting",
+      date: "12.00 - 13.00 pm",
     },
   ];
 
@@ -51,22 +61,23 @@ const Highlight = () => {
         <div className="highlight-left">
           <h2 className="highlight-left-title">Welcome to Ronald Board!</h2>
           <div className="highlight-left-wrapper">
-            
-          {dataLeft &&
-            dataLeft.map((data: any) => {
-              return (
-                <div className="highlight-left-card">
-                  <p className="highlight-left-card-title">{data.title}</p>
-                  <p className="highlight-left-card-desc">{data.description}</p>
-                </div>
-              );
-            })}
-          <hr className="highlight-divider"/>
-          <div className="highlight-left-card-button-container">
-            <button className="highlight-left-card-button">
-              Getting Started
-            </button>
-          </div>
+            {dataLeft &&
+              dataLeft.map((data: any) => {
+                return (
+                  <div className="highlight-left-card">
+                    <p className="highlight-left-card-title">{data.title}</p>
+                    <p className="highlight-left-card-desc">
+                      {data.description}
+                    </p>
+                  </div>
+                );
+              })}
+            <hr className="highlight-divider" />
+            <div className="highlight-left-card-button-container">
+              <button className="highlight-left-card-button">
+                Getting Started
+              </button>
+            </div>
           </div>
         </div>
         <div className="highlight-mid">
@@ -75,9 +86,10 @@ const Highlight = () => {
               return (
                 <div className="highlight-mid-card">
                   <div className="highlight-mid-card-container-header">
-                    <p className="highlight-mid-card-header-logo">
-                      {data.iconLogo}
-                    </p>
+                    <FaSlack
+                      className="highlight-mid-card-header-logo"
+                      size={32}
+                    />
                     <div className="highlight-mid-card-header-container-title">
                       <p className="highlight-mid-card-header-text-title">
                         {data.title}
@@ -99,13 +111,16 @@ const Highlight = () => {
                     {data.percentage}
                   </p>
                   <div className="highlight-mid-card-container-footer">
-                    <p className="highlight-mid-card-footer-iconProfile">
-                      {data.iconProfile}
-                    </p>
+                    <img
+                      src={userImg}
+                      width={100}
+                      className="highlight-mid-card-footer-iconProfile"
+                    />
                     <div className="highlight-mid-card-footer-container-right">
-                      <p className="highlight-mid-card-footer-iconDelete">
-                        icon
-                      </p>
+                      <MdDelete
+                        className="highlight-mid-card-footer-iconDelete"
+                        size={32}
+                      />
                       <p className="highlight-mid-card-footer-startDate">
                         Start {data.startDay} Nov
                       </p>
